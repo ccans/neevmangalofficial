0e25f66ce77db2682ba5160bb7f4606056ca5f66
ccans <55026208+ccans@users.noreply.github.com>
2021-07-03 00:47:25 -0500

    touch up

--- components/BlogCardContainer.js
+++ components/BlogCardContainer.js
@@ -1,14 +1,18 @@
 import BlogCardWrapper from "./BlogCardWrapper"
 // in the future, this should dynamically create blogcards and put them on the page instead of just having a static amount
 // Make sure to get the "loading" animation (make a loading blogcard) when needed and the pop-up animation too
+import "@fontsource/bebas-neue";
 
 function BlogCardContainer() {
 
     const elements = [<BlogCardWrapper id="1" />, <BlogCardWrapper id="2" />, <BlogCardWrapper id="3" />, <BlogCardWrapper id="4" />];
 
     return (
-        <div className="block flex-grow pt-4">
-            {/* Something like "recent blog entries" at the top here */}
+        <div className="block flex-grow pt-4 overflow-hidden" style={{boxShadow: "0 8px 6px -6px black"}}>
+
+            <div className="pl-10" > 
+                <h1 className="text-7xl tracking-wide" style={{fontFamily: "Bebas Neue"}}> Blog </h1>
+            </div>
             {elements}
         </div>
     )
--- components/MainStripe.js
+++ components/MainStripe.js
@@ -3,9 +3,9 @@ import "@fontsource/rajdhani";
 
 function MainStripe() {
     return (
-        <div className="z-20 flex w-screen md:justify-end justify-center w-screen md:w-2/5 overflow-hidden">
+        <div className="z-20 flex w-screen md:justify-end justify-center w-screen md:w-1/3 overflow-hidden">
             <div className="h-screen w-3/4 md:w-4/5 lg:w-3/5 bg-black float-right p-4">
-                <h1 className="text-white text-7xl" style={{fontFamily: "Bebas Neue"}}> Neev Mangal</h1>
+                <h1 className="text-white text-7xl tracking-wide" style={{fontFamily: "Bebas Neue"}}> Neev Mangal</h1>
                 <h3 className="text-white text-xl tracking-wider mb-2" style={{fontFamily: "Rajdhani"}}> Thinker ● Designer ● Creator </h3>
                 <hr style={{color: "white"}} />
 
