5851348a9ce48d3e5308c86cca2cf08303360bfc
ccans <55026208+ccans@users.noreply.github.com>
2021-07-06 23:52:59 -0400

    changes, fix mobile of blog page

---
+++ components/BlogComponents/BlogCardContainer.js
@@ -0,0 +1,9 @@
+function BlogCardContainer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BlogCardContainer
--- components/BlogStripe.js
+++ components/BlogStripe.js
@@ -37,9 +37,28 @@ function BlogStripe() {
                 }
 
             }}>
-                <h3  className="lg:text-3xl md:text-2xl text-xl" style={{fontFamily: "Rajdhani", maxWidth: "75%"}}> No, this isn’t a place where I tell you how transitioning to 5 seconds of screen time a day changed my life or any superficial unnatainnable lifestyle nonsense that most blogs are for. I write exclusively about my thoughts on everything, and, to a lesser extent, my hobbies. 
+                <h3  className="lg:text-3xl md:text-2xl text-xl" style={{fontFamily: "Rajdhani", maxWidth: "75%"}}> No, I'm not going to tell you how transitioning to 5 seconds of screen time a day changed my life or any superficial unnatainnable lifestyle nonsense that most blogs are for. I write exclusively about my thoughts on everything, and, to a lesser extent, my hobbies. 
  
 </h3>
+
+
+            </motion.div>
+            <motion.div className="text-white mt-10" initial="hidden" animate="visible" variants={{
+                hidden: {
+                    translateY: 200,
+                    opacity: 0
+                },
+                visible: {
+                    translateY: 0,
+                    opacity: 1,
+                    transition: {
+                        delay: .8,
+                        duration: .7
+                    }
+                }
+
+            }}>
+            <h4 className="lg:text-xl md:text-lg text-md font-semibold mt-4 text-gray-300" style={{fontFamily: "Rajdhani", maxWidth: "75%"}}> Ataraxia <em>(ancient greek)</em>: the lack of agitation that comes from understanding the ways of the universe, accepting fate, knowing what one can control and, therefore focusing only on the things one can actually change. </h4>
 
             </motion.div>
             
