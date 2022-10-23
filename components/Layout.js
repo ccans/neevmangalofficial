3525cc96daf069247f321f369d66d0356793d078
ccans <55026208+ccans@users.noreply.github.com>
2022-10-22 22:43:44 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 export default function Layout({ children, router }) {
 
-    const [droppedDown, toggleDropDown] = useState(true);
+    const [droppedDown, toggleDropDown] = useState(false);
 
         return (
             <div style={{backgroundColor: "navajowhite"}}>
