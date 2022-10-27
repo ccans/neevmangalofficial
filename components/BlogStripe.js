19599caf657191ef30c3a29571cdd9920ea788ba
ccans <55026208+ccans@users.noreply.github.com>
2022-10-26 21:52:16 -0500

    Update BlogStripe.js

--- components/BlogStripe.js
+++ components/BlogStripe.js
@@ -40,7 +40,7 @@ function BlogStripe() {
             }}>
             <h4 className="lg:text-xl md:float-left md:text-lg text-md mt-4 text-gray-300 m-auto md:text-left text-center text-black" style={{fontFamily: "monospace", maxWidth: "75%"}}> Hi! My name is Neev Mangal and I’m a senior at Wayzata High School in Plymouth, Minnesota, where I have lived for seven years. I’ve had a wide array of interests throughout the years—solving Rubik’s Cubes, playing tennis, and capturing astrophotography—to name some. Throughout my high school life, however, I have discovered a love for robotics, debate, and research. I captain both my school’s robotics and debate teams and enjoy both activities thoroughly (luckily, one has a Fall season while the other occurs during the Winter/Spring). My research work spans from detecting exoplanets for the American Association of Variable Star Observers (AAVSO) to geospatial analysis of the planet to help identify ocean plastics under a professor at the University of Minnesota.
 <br /> <br />
-In my spare time, I enjoy listening to music, writing (political and apolitical) blog pieces, and programming. Programming has been a large part of my life for a long time, so I decided to bring everything I’d learned together into a personal websiten—a <strong> magnum opus. </strong>
+In my spare time, I enjoy listening to music, writing (political and apolitical) blog pieces, and programming. Programming has been a large part of my life for a long time, so I decided to bring everything I’d learned together into a personal website—a <strong> magnum opus. </strong>
 <br /> <br />
 My writing and astrophotography are my favorite parts of this website. I also have a few project write-ups
 <br /> <br />
